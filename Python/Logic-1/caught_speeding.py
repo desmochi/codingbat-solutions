197e8f55e7e05f72f90d1bea74d9249473c5849c
desmochi <44042133+desmochi@users.noreply.github.com>
2018-10-30 08:54:12 -0700

    Another way to complete the task
    Here's an alternative way to complete the task on codingbat.com

--- Python/Logic-1/caught_speeding.py
+++ Python/Logic-1/caught_speeding.py
@@ -8,11 +8,17 @@ caught_speeding(65, True) → 0
 
 
 def caught_speeding(speed, is_birthday):
-
-    if is_birthday:
-        speed -= 5
-
-    if speed <= 60:
-        return 0
-
-    return 1 if 61 <= speed <= 80 else 2
+    if (is_birthday):
+        if (speed <= 65):
+            return 0
+        elif (66 <= speed <= 85):
+            return 1
+        else:
+            return 2
+    else:
+        if (speed <= 60):
+            return 0
+        elif (61 <= speed <= 80):
+            return 1
+        else:
+            return 2
